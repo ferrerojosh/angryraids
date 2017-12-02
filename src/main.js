50fd02cf49ea3e40bd2429ba7a97594c85ea1187
John Joshua Ferrer <johnjoshuaferrer@gmail.com>
2017-12-02 23:36:34 +0800

    Add routes to heroes

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import { sync } from 'vuex-router-sync'
 import router from './router'
 import store from './store'
+
+sync(store, router)
 
 Vue.config.productionTip = false
 

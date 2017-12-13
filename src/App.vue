9076138fda855cc3ee2fef2146bb6f2954792fba
John Joshua Ferrer <johnjoshuaferrer@gmail.com>
2017-12-13 23:39:07 +0800

    0.1.8

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,23 @@
       <header class="section">
         <h1 class="title">AngryRaids</h1>
         <h2 class="subtitle">King's Raid Equipment Simulator</h2>
-        <small>Release 0.1.7. In need of data in order to pursue further development, please
-          <a href="https://github.com/ferrerojosh/angryraids/issues" target="_blank">submit an issue on github</a>
-          to help.
-        </small>
+        <small>Release 0.1.8</small>
+
       </header>
       <div class="is-clearfix"></div>
+      <div class="changes">
+        <p>What's new?</p>
+        <ul>
+          <li>Recently fixed a wrong calculation on applying stat modifiers.</li>
+        </ul>
+      </div>
+
       <router-view></router-view>
     </main>
     <footer class="github-footer">
       <a href="https://github.com/ferrerojosh/angryraids" target="_blank">
-        <i class="fa fa-4x fa-github"></i>
-        <p>Freshly <img width="16" height="16" src="/static/img/icons/bread.svg"> baked by Gavenda</p>
+        <i class="fa fa-3x fa-github"></i>
+        <p>Freshly <img width="24" height="24" src="/static/img/icons/bread.svg"> baked by Gavenda</p>
       </a>
     </footer>
   </div>
@@ -32,10 +37,21 @@
 <style lang="sass">
   .github-footer
     text-align: center
-    margin: 2rem
+    margin: 2.5rem
     > a
       color: #000000
       > p
         color: #666
         font-size: 0.8rem
+
+  .changes
+    margin-left: 2rem
+    margin-bottom: 2rem
+
+    > ul
+      list-style-type: square
+      margin-top: 1em
+      > li
+        font-size: 14px
+        margin-left: 2em
 </style>

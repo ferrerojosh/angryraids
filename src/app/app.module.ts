749922fb4d9d49252e21aac4f96c766bf82573d3
John Joshua Ferrer <johnjoshuaferrer@gmail.com>
2019-01-31 12:53:21 +0800

    Add initial pages: Home, Simulator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SimulatorComponent } from './pages/simulator/simulator.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SimulatorComponent
   ],
   imports: [
     BrowserModule,

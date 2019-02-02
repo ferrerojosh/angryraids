409cb7e95ad6cf7b421534e27384c9c68a17f679
John Joshua Ferrer <johnjoshuaferrer@gmail.com>
2019-02-02 14:44:30 +0800

    Autocomplete: Add docs

--- src/app/modules/auto-complete/components/auto-complete-item/auto-complete-item.component.ts
+++ src/app/modules/auto-complete/components/auto-complete-item/auto-complete-item.component.ts
@@ -1,20 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+/**
+ * @description
+ * A very simple implementation of an auto complete item.
+ */
 @Component({
   selector: 'app-auto-complete-item',
   templateUrl: './auto-complete-item.component.html',
   styleUrls: ['./auto-complete-item.component.styl']
 })
-export class AutoCompleteItemComponent implements OnInit {
-
+export class AutoCompleteItemComponent {
+  /**
+   * The string that is being focused upon, it is shown in bold text.
+   */
   @Input()
   focused;
+  /**
+   * The string that is not focused, not in bold.
+   */
   @Input()
   value;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
 }
--- src/app/modules/auto-complete/containers/auto-complete/auto-complete.component.ts
+++ src/app/modules/auto-complete/containers/auto-complete/auto-complete.component.ts
@@ -2,6 +2,10 @@ import { Component, HostListener, Input, OnDestroy, OnInit } from '@angular/core
 import { FormControl, FormGroup } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
+/**
+ * @description
+ * An simple reactive auto complete component.
+ */
 @Component({
   selector: 'app-auto-complete',
   templateUrl: './auto-complete.component.html',
@@ -9,10 +13,17 @@ import { Subscription } from 'rxjs';
 })
 export class AutoCompleteComponent implements OnInit, OnDestroy {
 
+  /**
+   * Component placeholder text.
+   */
   @Input()
   placeholder = '';
+  /**
+   * The array of data strings to be filtered.
+   */
   @Input()
   data: string[];
+
   focused = false;
   value = '';
 
@@ -22,19 +33,35 @@ export class AutoCompleteComponent implements OnInit, OnDestroy {
   valueChangesSubscription: Subscription;
 
   ngOnInit() {
+    // Listen to input value changes
     this.valueChangesSubscription = this.autoCompleteForm.valueChanges
       .subscribe(value => this.onValueChange(value.searchInput));
   }
 
-  focusedStr(str) {
+  /**
+   * Returns the focused string based on the component's input value.
+   * @param str the string to check on
+   */
+  focusedStr(str): string {
     return str.substr(0, this.value.length);
   }
 
-  unfocusedStr(str) {
+  /**
+   * Returns the unfocused string based on the component's input value.
+   * @param str the string to check on
+   */
+  unfocusedStr(str): string {
     return str.substr(this.value.length);
   }
 
-  get filteredData() {
+  /**
+   * Returns the filtered data based on user input.
+   *
+   * @usageNotes
+   * It currently sorts alphabetically, this component can be further improved upon by adding
+   * a sorting strategy.
+   */
+  get filteredData(): string[] {
     const filteredData = this.data
       .filter(c => c
         .toString()
@@ -54,6 +81,9 @@ export class AutoCompleteComponent implements OnInit, OnDestroy {
     this.focused = true;
   }
 
+  /**
+   * Hides the auto complete list when clicking anywhere on the document.
+   */
   @HostListener('document:click')
   onDocumentClick() {
     this.focused = false;

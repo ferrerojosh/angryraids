50add7cf8da92b231fa9667004c5bccdc6e08e19
John Joshua Ferrer <johnjoshuaferrer@gmail.com>
2019-02-03 06:45:03 +0800

    Add unit tests

--- src/app/modules/kings-raid/services/hero-builder.service.spec.ts
+++ src/app/modules/kings-raid/services/hero-builder.service.spec.ts
@@ -3,7 +3,9 @@ import { TestBed } from '@angular/core/testing';
 import { HeroBuilder } from './hero-builder.service';
 
 describe('HeroBuilderService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [HeroBuilder]
+  }));
 
   it('should be created', () => {
     const service: HeroBuilder = TestBed.get(HeroBuilder);
--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AutoCompleteModule } from '../../modules/auto-complete/auto-complete.module';
 
 import { HomeComponent } from './home.component';
 
@@ -8,6 +9,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ AutoCompleteModule ],
       declarations: [ HomeComponent ]
     })
     .compileComponents();

75b59de2245a09073dbde75764a15defd35b7ba7
John Joshua Ferrer <johnjoshuaferrer@gmail.com>
2019-03-05 06:35:55 +0800

    Don't log webp script

--- scripts/asset-to-webp.sh
+++ scripts/asset-to-webp.sh
@@ -2,9 +2,9 @@
 # convert assets to webp format
 cwd=$(pwd)
 cd ${cwd}/src/assets/images/hero/
-find . -iname "image.png" -execdir cwebp '{}' -o image.webp  \;
-find . -iname "unique-treasure-1.png" -execdir cwebp '{}' -o unique-treasure-1.webp  \;
-find . -iname "unique-treasure-2.png" -execdir cwebp '{}' -o unique-treasure-2.webp  \;
-find . -iname "unique-treasure-3.png" -execdir cwebp '{}' -o unique-treasure-3.webp  \;
-find . -iname "unique-treasure-4.png" -execdir cwebp '{}' -o unique-treasure-4.webp  \;
-find . -iname "unique-weapon.png" -execdir cwebp '{}' -o unique-weapon.webp \;
+find . -iname "image.png" -execdir cwebp '{}' -o image.webp  -quiet \;
+find . -iname "unique-treasure-1.png" -execdir cwebp '{}' -o unique-treasure-1.webp -quiet  \;
+find . -iname "unique-treasure-2.png" -execdir cwebp '{}' -o unique-treasure-2.webp -quiet  \;
+find . -iname "unique-treasure-3.png" -execdir cwebp '{}' -o unique-treasure-3.webp -quiet  \;
+find . -iname "unique-treasure-4.png" -execdir cwebp '{}' -o unique-treasure-4.webp -quiet  \;
+find . -iname "unique-weapon.png" -execdir cwebp '{}' -o unique-weapon.webp  -quiet \;

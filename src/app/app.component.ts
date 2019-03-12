4ec50711854edf5fae6f60e3321c01ef2d063df0
John Joshua Ferrer <johnjoshuaferrer@gmail.com>
2019-03-13 07:18:27 +0800

    Add service worker update notice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+
+  swSubscription: Subscription;
+  updated = false;
+
+  constructor(
+    private readonly swUpdate: SwUpdate,
+  ) {
+  }
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swSubscription = this.swUpdate.available.subscribe(() => this.updated = true);
+      this.swUpdate.checkForUpdate().then(() => console.log('Finish checking for updates.'));
+    }
+  }
+
+  doUpdate() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.activateUpdate().then(() => window.location.reload());
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.swUpdate.isEnabled) {
+      this.swSubscription.unsubscribe();
+    }
+  }
 }
